6e5d00336d54b89f0be157940328f77fc685cfc3
typescriptkamran <typescriptwithkamran@gmail.com>
2023-08-01 23:06:55 +0500

    test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
-// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+// Exsersise 2: Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
 let personName = "Kamran";
-let message = `Hello ${personName}, hope you are doing will these days.`;
+let message = `Hello ${personName},hope you are doing will these days.`;
 console.log(message);
